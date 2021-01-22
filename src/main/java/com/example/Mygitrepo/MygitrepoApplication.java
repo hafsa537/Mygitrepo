2aa51100043f1c136d1f928a2c8878583257ad20
hafsa537 <hafsa.star@gmail.com>
2021-01-22 20:31:17 +0530

    first commit

---
+++ src/main/java/com/example/Mygitrepo/MygitrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mygitrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygitrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mygitrepo/MygitrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mygitrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygitrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
